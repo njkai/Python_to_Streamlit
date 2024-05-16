40a079233d92b211124c2efe4e3bb9d6a9dbc9b3
njkai <nkaiser@n-s-i.us>
2024-05-16 16:57:22 -0400

    Update test.py

--- test.py
+++ test.py
@@ -7,12 +7,3 @@ if st.checkbox('Show dataframe'):
        np.random.randint(0,50, size=(10,3)), columns=['FP101', 'FP201', 'FP301'])
     chart_data.index=['2014','2015','2016','2017','2018','2019','2020','2021','2022','2023']
     chart_data
-    
-import polars as pl
-
-if st.checkbox('Show parquet shape'):
-    df = pl.read_parquet("~\\Downloads\\TOMAHAWK_ALL_LABOR.parquet")
-    df.shape
-    df.head(1)
-    df
-    
