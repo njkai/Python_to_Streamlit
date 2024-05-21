7435649968c037fd00ec65125aafd7a594ca92f5
njkai <nkaiser@n-s-i.us>
2024-05-21 11:27:19 -0400

    Delete test.py

--- test.py
+++
@@ -1,9 +0,0 @@
-import streamlit as st
-import pandas as pd
-import numpy as np
-
-if st.checkbox('Show dataframe'):
-    chart_data = pd.DataFrame(
-       np.random.randint(0,50, size=(10,3)), columns=['FP101', 'FP201', 'FP301'])
-    chart_data.index=['2014','2015','2016','2017','2018','2019','2020','2021','2022','2023']
-    chart_data
